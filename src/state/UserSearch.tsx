c08a537d00996b930bd83c7551c28aefcc2edaea
jimymltta <marlettajimy@gmail.com>
2021-11-11 16:22:01 +0100

    initial

---
+++ src/state/UserSearch.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+const users = [
+  { name: 'Iléana', age: 23 },
+  { name: 'Ragnar', age: 4 },
+  { name: 'Oscar', age: 1 },
+];
+
+const UserSearch: React.FC = () => {
+  const [name, setName] = useState('');
+  const [user, setUser] = useState<{ name: string; age: number } | undefined>();
+
+  const onClick = () => {
+    const foundUser = users.find((user) => {
+      return user.name === name;
+    });
+    setUser(foundUser);
+  };
+
+  return (
+    <div>
+      User search
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={onClick}>Find User</button>
+      <div>
+        {user && user.name}
+        {user && user.age}
+      </div>
+    </div>
+  );
+};
+
+export default UserSearch;
