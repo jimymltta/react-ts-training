5141006b1d5eb12c66c42718d640b1056e9615a5
jimymltta <marlettajimy@gmail.com>
2021-11-17 11:26:14 +0100

    continued exercices, using useEffect, useRef etc with TS

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import ReactDOM from 'react-dom';
 import GuestList from './state/GuestList';
-import UserSearch from './state/UserSearch';
+// import UserSearch from './state/UserSearch';
+import EventComponent from './events/EventComponent';
+import UserSearch from './refs/UserSearch';
 
 const App = () => {
   return (
     <div>
       <GuestList />
+      <EventComponent />
       <UserSearch />
     </div>
   );
